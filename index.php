3ba445f14a9f2e2ebcf8be7778511318554454d0
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-07 15:48:45 +0100

    Update de l'index.php et index.css ,2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ head($page); ?>
 
     <main>
         <div class="brown-bar"></div>
-        <section id="SectionACF">
+        <section id="sectionACF">
             <!-- ACCOMPAGNEMENT/COMPETENCES/FORMATION -->
             <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
                     <h2>MON TITRE</h2>-->
@@ -15,63 +15,63 @@ head($page); ?>
                 <div class="row"> <!-- ça aligne -->
                     <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
                         <div class="d-flex justify-content-center">
-                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                            <img src="https://placehold.co/150x150" class="m-3" height="150px" width="150px" alt="kc">
                         </div>
-                        <div class="d-flex justify-content-center">
+                        <div class="d-flex justify-content-center m-3">
                             ici mon titre 1
                         </div>
-                        <div class="">
+                        <div class="m-3">
                             ici mon contenu 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus dolorum repudiandae sunt excepturi nobis quo, hic, voluptatum autem recusandae doloremque at? Dolore voluptatibus velit quia, aliquid debitis nisi veritatis accusantium.
                         </div>
                     </div>
                     <div class="col-lg-4 border-end border-start"> <!-- On copie ça le nombre de fois ou on a besoin -->
                         <div class=" d-flex justify-content-center">
-                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                            <img src="https://placehold.co/150x150" class="m-3" height="150px" width="150px" alt="kc">
                         </div>
-                        <div class="d-flex justify-content-center">
+                        <div class="d-flex justify-content-center m-3">
                             ici mon titre 2
                         </div>
-                        <div class="">
+                        <div class="m-3">
                             ici mon contenu 2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae ea itaque odit velit animi magni cum. Distinctio est eligendi sint vel, pariatur blanditiis reprehenderit quidem voluptates itaque eveniet nam assumenda!
                         </div>
                     </div>
                     <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
                         <div class="d-flex justify-content-center">
-                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                            <img src="https://placehold.co/150x150" class="m-3" height="150px" width="150px" alt="kc">
                         </div>
-                        <div class="d-flex justify-content-center">
+                        <div class="d-flex justify-content-center m-3">
                             ici mon titre 3
                         </div>
-                        <div class="">
+                        <div class="m-3">
                             ici mon contenu 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sint, architecto in accusantium neque cum perferendis et vel, possimus voluptatibus iusto ullam sed id optio culpa consequatur. Expedita, natus distinctio.
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section>
+        <section id="sectionCL">
             <!-- CADRE LEGALE -->
             <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
                     <h2>MON TITRE</h2>-->
 
                 <div class="row"> <!-- ça aligne -->
                     <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="">
-                            ici mon titre
+                        <div class="m-3 fontCadreLegale">
+                            CADRE <br> LEGALE
                         </div>
                     </div>
                     <div class="col-lg-6 border-start"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="">
-                            ici mon contenu
+                        <div class="m-3">
+                            Les entreprises ont pour obligations de mettre en place des formations de sécurité. Ces formations s'adressent aux personnes qui sont en CDD ou intérimaires, qui sont nouvellemnt embauchés, qui changent de poste, qui ont été en arrêt depuis 21 jours et pour les entreprises exterieurs. NR prévention intervient donc en tant que formateur indépendant pour assurer la mise en pratique et théorique de ces formations qui visent à protéger la santé et maintenir le bien-être au travail pour les salariés.
                         </div>
-                        <div class="">
-                            ici mon contenu
+                        <div class="m-3">
+                            Article L 4141-2 du code du travail
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section>
+        <section id="sectionF">
             <!-- FORMATIONS -->
             <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
                     <h2>MON TITRE</h2>-->
@@ -119,7 +119,7 @@ head($page); ?>
                 </div>
             </div>
         </section>
-        <section>
+        <section id="sectionQ">
             <!-- Qualiopi -->
             <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
                     <h2>MON TITRE</h2>-->
@@ -159,18 +159,18 @@ head($page); ?>
                 </div>
             </div>
         </section>
-        <section>
+        <section id="sectionCD">
             <!-- Calendrier/Devis -->
             <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
                     <h2>MON TITRE</h2>-->
                 <div class="row"> <!-- ça aligne -->
-                    <div class="col-lg-2 d-flex align-items-center"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border d-flex justify-content-center">
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center align-items-center">
                             <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
                         </div>
                     </div>
                     <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border d-flex justify-content-center align-items-center">
+                        <div class="border d-flex justify-content-center">
                             ici mon contenu
                         </div>
                     </div>
@@ -190,6 +190,7 @@ head($page); ?>
     </main>
 
     <footer> <?php footer(); ?> </footer>
+
 </body>
 
 </html>
