ef115b9b7f055531d49e184941aa0142d9ae8434
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-07 12:22:11 +0100

    Update de l'index

--- index.php
+++ index.php
@@ -1,11 +1,197 @@
-<?php include "app/app.php"; $page = 1; head($page); ?>
-<body> <header> <?php navbar($page); ?> </header>
+<?php include "app/app.php";
+$page = 1;
+head($page); ?>
 
-<main>
+<body>
+    <header> <?php navbar($page); ?> </header>
 
-    <p>Exemple </p>
+    <main>
 
-</main>
+        <p>Exemple </p>
+
+        <section id="SectionACF">
+            <!-- ACCOMPAGNEMENT/COMPETENCES/FORMATION -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
+                    <h2>MON TITRE</h2>-->
+
+                <div class="row"> <!-- ça aligne -->
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="d-flex justify-content-center">
+                            ici mon image 1
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            ici mon titre 1
+                        </div>
+                        <div class="">
+                            ici mon contenu 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus dolorum repudiandae sunt excepturi nobis quo, hic, voluptatum autem recusandae doloremque at? Dolore voluptatibus velit quia, aliquid debitis nisi veritatis accusantium.
+                        </div>
+                    </div>
+                    <div class="col-lg-4 border-end border-start"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class=" d-flex justify-content-center">
+                            ici mon image 2
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            ici mon titre 2
+                        </div>
+                        <div class="">
+                            ici mon contenu 2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae ea itaque odit velit animi magni cum. Distinctio est eligendi sint vel, pariatur blanditiis reprehenderit quidem voluptates itaque eveniet nam assumenda!
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="d-flex justify-content-center">
+                            ici mon image 3
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            ici mon titre 3
+                        </div>
+                        <div class="">
+                            ici mon contenu 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sint, architecto in accusantium neque cum perferendis et vel, possimus voluptatibus iusto ullam sed id optio culpa consequatur. Expedita, natus distinctio.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <!-- CADRE LEGALE -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
+                    <h2>MON TITRE</h2>-->
+
+                <div class="row"> <!-- ça aligne -->
+                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="">
+                            ici mon titre
+                        </div>
+                    </div>
+                    <div class="col-lg-6 border-start"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="">
+                            ici mon contenu
+                        </div>
+                        <div class="">
+                            ici mon contenu
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <!-- FORMATIONS -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
+                    <h2>MON TITRE</h2>-->
+                <div class="row justify-content-between"> <!-- ça aligne -->
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border">
+                            <img src="https://placehold.co/200x400" height="400px" width="200px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon
+                        </div>
+                    </div>
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/200x400" height="400px" width="200px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border">
+                            <img src="https://placehold.co/200x400" height="400px" width="200px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon image
+                        </div>
+                    </div>
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border">
+                            <img src="https://placehold.co/200x400" height="400px" width="200px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border">
+                            <img src="https://placehold.co/200x400" height="400px" width="200px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <!-- Qualiopi -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
+                    <h2>MON TITRE</h2>-->
+                <div class="row justify-content-between"> <!-- ça aligne -->
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                        <div class="border d-flex justify-content-center">
+                            ici mon rectangle
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <!-- Calendrier/Devis -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)
+                    <h2>MON TITRE</h2>-->
+                <div class="row"> <!-- ça aligne -->
+                    <div class="col-lg-2 d-flex align-items-center"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center align-items-center">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center align-items-center">
+                            <img src="https://placehold.co/150x150" height="150px" width="150px" alt="kc">
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border d-flex justify-content-center">
+                            ici mon contenu
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 
-</body> </html>
+</body>
+
+</html>
