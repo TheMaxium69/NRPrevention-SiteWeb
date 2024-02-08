b11a1ee20f1e42283aeff66390ed7f4f2f4575b2
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-08 16:03:43 +0100

    mise en page de formation.php

---
+++ formation.php
@@ -0,0 +1,55 @@
+<?php include "app/app.php";
+$page = 3;
+head($page); ?>
+
+<header> <?php navbar($page); ?> </header>
+
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer> <?php footer(); ?> </footer>
+</html>
+</body>
