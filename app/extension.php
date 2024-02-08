c63558d10c0dde8013dff21543370816e0261433
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-08 10:47:26 +0100

    Finish footer 99%

--- app/extension.php
+++ app/extension.php
@@ -2,4 +2,6 @@
 include "env.php";
 
 require_once $env_urlFile . "Boostrap.phtml";
+require_once $env_urlFile . "googlefont.phtml";
+require_once $env_urlFile . "fontawasome.phtml";
 require_once $env_urlFile . "link.phtml";
