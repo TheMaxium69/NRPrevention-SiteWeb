7cab5199cfe74c4e6d8059aa37031e3fa58882c9
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-09 10:40:22 +0100

    Update css index (manque les images)

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ head($page); ?>
                             <img src="./assets/deggr.png" class="m-3" height="200px" width="200px" alt="kc">
                         </div>
                         <div class="d-flex justify-content-center m-3">
-                            ici mon titre 1
+                            <p class="fontOrange">Accompagnement</p>
                         </div>
                         <div class="m-3">
-                            ici mon contenu 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus dolorum repudiandae sunt excepturi nobis quo, hic, voluptatum autem recusandae doloremque at? Dolore voluptatibus velit quia, aliquid debitis nisi veritatis accusantium.
+                            <p> Spécialiste de la prévention et de la sécurité en entreprises,nous vous accompagnons dans la formation de vos salariés en satsifaisant aux obligations légales et règlementaires en vigueur en fonction de vos besoins.</p>
                         </div>
                     </div>
                     <div class="col-lg-4 border-end border-start"> <!-- On copie ça le nombre de fois ou on a besoin -->
@@ -29,10 +29,10 @@ head($page); ?>
                             <img src="./assets/iohni.png" class="m-3" height="200px" width="200px" alt="deggr">
                         </div>
                         <div class="d-flex justify-content-center m-3">
-                            ici mon titre 2
+                        <p class="fontOrange">Compétences</p>
                         </div>
                         <div class="m-3">
-                            ici mon contenu 2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae ea itaque odit velit animi magni cum. Distinctio est eligendi sint vel, pariatur blanditiis reprehenderit quidem voluptates itaque eveniet nam assumenda!
+                            Notre objectif est d'apporter à vos collaborateurs les compétences nécessaires en matière de secourisme et/ou de sécurité incendie afin de pouvoir intervenir rapidement et efficacement.
                         </div>
                     </div>
                     <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
@@ -40,10 +40,10 @@ head($page); ?>
                             <img src="./assets/sqfze.png" class="m-3" height="200px" width="200px" alt="sqfze">
                         </div>
                         <div class="d-flex justify-content-center m-3">
-                            ici mon titre 3
+                        <p class="fontOrange">Formation</p>
                         </div>
                         <div class="m-3">
-                            ici mon contenu 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sint, architecto in accusantium neque cum perferendis et vel, possimus voluptatibus iusto ullam sed id optio culpa consequatur. Expedita, natus distinctio.
+                            Nos formations sont dispensés par des professionnels de l'incendie et du secourisme. Elles sont composées d'apports théoriques interactifs et de participatifs ainsi que d'études de cas et de mises en pratique.
                         </div>
                     </div>
                 </div>
@@ -76,7 +76,7 @@ head($page); ?>
 
                 <div class="row"> <!-- ça aligne -->
                     <div class="col-lg-6 h2NR"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div id="h2CL" class="m-3 fontCadreLegale">
+                        <div id="h2CL" class="m-3 fontCadreLegale fontOrange">
                             CADRE <br> LEGALE
                             <style>
                                 .h2NR:before {
@@ -90,7 +90,7 @@ head($page); ?>
                         <div class="m-3">
                             Les entreprises ont pour obligations de mettre en place des formations de sécurité. Ces formations s'adressent aux personnes qui sont en CDD ou intérimaires, qui sont nouvellemnt embauchés, qui changent de poste, qui ont été en arrêt depuis 21 jours et pour les entreprises exterieurs. NR prévention intervient donc en tant que formateur indépendant pour assurer la mise en pratique et théorique de ces formations qui visent à protéger la santé et maintenir le bien-être au travail pour les salariés.
                         </div>
-                        <div class="m-3">
+                        <div class="m-3 fontOrange">
                             Article L 4141-2 du code du travail
                         </div>
                     </div>
@@ -114,43 +114,43 @@ head($page); ?>
                     <h2>MON TITRE</h2>-->
                 <div class="row d-flex justify-content-around imageFormation"> <!-- ça aligne -->
                     <div class="col-lg-2 custom-width"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border d-flex justify-content-center">
-                            <img class="img-fluid" src="./assets/page-9.jpeg" alt="kc">
+                        <div class="border d-flex justify-content-center imgF1">
+
                         </div>
                         <div class="border d-flex justify-content-center">
-                            <p class="text-start">Formation <br> Incendie</p>
+                            <p class="text-start fontOrange">Formation <br> Incendie</p>
                         </div>
                     </div>
                     <div class="col-lg-2 custom-width"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border">
-                            <img src="./assets/postures.jpeg" alt="kc">
+                        <div class="border imgF2">
+
                         </div>
                         <div class="border d-flex justify-content-center">
-                            <p class="text-start">Formation <br> Geste et posture</p>
+                            <p class="text-start fontOrange">Formation <br> Geste et posture</p>
                         </div>
                     </div>
                     <div class="col-lg-2 custom-width"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border">
-                            <img src="./assets/dfdfg (1).jpeg" alt="kc">
+                        <div class="border imgF3">
+
                         </div>
                         <div class="border d-flex justify-content-center">
-                            <p class="text-start">Formation <br> SST</p>
+                            <p class="text-start fontOrange">Formation <br> SST</p>
                         </div>
                     </div>
                     <div class="col-lg-2 custom-width"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border">
-                            <img src="./assets/defib.jpeg" alt="kc">
+                        <div class="border imgF4">
+
                         </div>
                         <div class="border d-flex justify-content-center">
-                            <p class="text-start">Formation <br> Défibrilateur</p>
+                            <p class="text-start fontOrange">Formation <br> Défibrilateur</p>
                         </div>
                     </div>
                     <div class="col-lg-2 custom-width"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="border">
-                            <img src="./assets/page-5.jpeg" alt="kc">
+                        <div class="border imgF5">
+
                         </div>
                         <div class="border d-flex justify-content-center">
-                            <p class="text-start">Formation <br>MAC SST</p>
+                            <p class="text-start fontOrange">Formation <br>MAC SST</p>
                         </div>
                     </div>
                 </div>
