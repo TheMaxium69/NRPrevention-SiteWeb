a94a94150717fda77e425d5ae40ca30f5a49b2fc
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-09 16:50:20 +0100

    update contact

--- contact.php
+++ contact.php
@@ -9,24 +9,23 @@ head($page); ?>
     <main>
 
         <section id="sectionCN">
-            <div class="blockCN">
-                <h1>CONTACTEZ-NOUS</h1>
-                <br>
-                <p>Notre équipe se tient à votre disposition pour toutes demandes ou conseils, n'hésitez pas.</p>
-            </div>
-            <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                <div class="border d-flex justify-content-center">
-                    <img class="img-fluid" src="./assets/calcul.png" alt="kc">
+            <div class="blockCN ">
+                <div class="col-lg-6">
+                    <h2 class="text-white">CONTACTEZ-NOUS</h2>
+                </div>
+                <div class="">
+                    <p class="">Notre équipe se tient à votre disposition pour toutes demandes ou conseils, n'hésitez pas.</p>
                 </div>
             </div>
-            <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                <div class="border d-flex justify-content-center">
-                    <h2>Devis</h2>
-                </div>
+        </section>
+        <section>
+            <div class="container">
+                <h2 class="fontForm">Ecrivez-nous un message</h2>
             </div>
+
+
 
         </section>
-
 
 
 
