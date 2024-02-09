1ece514931a8d8b9651c81c01beb6ea0c4bd2078
Maxime Tournier <mxm.tournier@gmail.com>
2024-02-09 15:27:43 +0100

    Update calendrier.php

--- calendrier.php
+++ calendrier.php
@@ -18,67 +18,12 @@ head($page); ?>
 
 
 
-    
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-    <main>
-        <div class="displayback-calendar">
-
-        </div>
-
-
-
-    </main>
->>>>>>> afc762b1f05d23af25115eab63a749829766fc16
->>>>>>> Stashed changes
 
 
     <footer> <?php footer(); ?> </footer>
@@ -87,64 +32,3 @@ head($page); ?>
 
 </html>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<footer> <?php footer(); ?> </footer>
-</body>
-
-</html>
