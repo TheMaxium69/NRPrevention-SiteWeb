6d61c933130abda4df7e43aa7a7f8af9bccdc878
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-12 10:25:17 +0100

    update calendar.php/css

--- calendrier.php
+++ calendrier.php
@@ -3,16 +3,50 @@ $page = 2;
 head($page); ?>
 
 <body>
-    <header> <?php navbar($page); ?> </header>
+    <header> <?php navbar($page); ?> 
+    </header>
 
 
 <main>
     <div class= "displayback-calendar">
+        </div>
+        <h1 class="margin-header-calendrier">Calendrier 2022</h1>
 
+    <div class="comportement-div">
+    
+    <div id="formation-incendie">
+        
+        <div class="planning">
+        <div class="titre-formation"></div>
+        </div>
+        <div class="lieu"></div>
+        
     </div>
-
-
-
+    <div id="formation-incendie">
+        
+        <div class="planning">
+        <div class="titre-formation"></div>
+        </div>
+        <div class="lieu"></div>
+        
+    </div>
+    <div id="formation-incendie">
+        
+        <div class="planning">
+        <div class="titre-formation"></div>
+        </div>
+        <div class="lieu"></div>
+        
+    </div>
+    <div id="formation-incendie">
+        
+        <div class="planning">
+        <div class="titre-formation"></div>
+        </div>
+        <div class="lieu"></div>
+        
+    </div>
+    </div>
 </main>
 
 
