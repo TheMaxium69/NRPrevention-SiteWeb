2f4db6ce286dd32c2fb65abe1a2303f006834fb6
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-12 12:09:43 +0100

    Update lien footer et page contact

--- calendrier.php
+++ calendrier.php
@@ -3,55 +3,55 @@ $page = 2;
 head($page); ?>
 
 <body>
-    <header> <?php navbar($page); ?> 
+    <header> <?php navbar($page); ?>
     </header>
 
 
-<main>
-    <div class= "displayback-calendar">
+    <main>
+        <div class="displayback-calendar">
         </div>
         <h1 class="margin-header-calendrier">Calendrier 2022</h1>
 
-    <div class="comportement-div">
-    
-    <div id="formation-incendie">
-        
-        <div class="planning">
-            
-        <div class="titre-formation">
-            <h3>Formation Incendie</h3>
+        <div class="comportement-div">
+
+            <div id="formation-incendie">
+
+                <div class="planning">
+
+                    <div class="titre-formation">
+                        <h3>Formation Incendie</h3>
+                    </div>
+                </div>
+
+                <div class="lieu"></div>
+
+            </div>
+            <div id="formation-incendie">
+
+                <div class="planning">
+                    <div class="titre-formation"></div>
+                </div>
+                <div class="lieu"></div>
+
+            </div>
+            <div id="formation-incendie">
+
+                <div class="planning">
+                    <div class="titre-formation"></div>
+                </div>
+                <div class="lieu"></div>
+
+            </div>
+            <div id="formation-incendie">
+
+                <div class="planning">
+                    <div class="titre-formation"></div>
+                </div>
+                <div class="lieu"></div>
+
+            </div>
         </div>
-        </div>
-        
-        <div class="lieu"></div>
-        
-    </div>
-    <div id="formation-incendie">
-        
-        <div class="planning">
-        <div class="titre-formation"></div>
-        </div>
-        <div class="lieu"></div>
-        
-    </div>
-    <div id="formation-incendie">
-        
-        <div class="planning">
-        <div class="titre-formation"></div>
-        </div>
-        <div class="lieu"></div>
-        
-    </div>
-    <div id="formation-incendie">
-        
-        <div class="planning">
-        <div class="titre-formation"></div>
-        </div>
-        <div class="lieu"></div>
-        
-    </div>
-    </div>
-</main>
+    </main>
 
 
 
@@ -69,4 +69,3 @@ head($page); ?>
 </body>
 
 </html>
-
--- contact.php
+++ contact.php
@@ -11,12 +11,12 @@ head($page); ?>
         <section id="sectionCN">
             <div class="container-fluid p-0">
                 <div class="row">
-                    <div class="col-lg-6 bg-warning">
-                        <h2 class="d-flex justify-content-center text-white p-4">CONTACTEZ NOUS</h2>
-                        <p class="d-flex justify-content-center p-4">Notre équipe se tient à votre disposition pour <br> toutes demandes ou conseils, n'hésitez pas.</p>
+                    <div class="col-lg-6 bgCN">
+                        <h2 class="d-flex justify-content-center text-white fontTitleCN pt-4">CONTACTEZ NOUS</h2>
+                        <p class="d-flex justify-content-center pb-4 fontTextCN">Notre équipe se tient à votre disposition pour <br> toutes demandes ou conseils, n'hésitez pas.</p>
                     </div>
-                    <div class="col-lg-6">
-                        <p class="d-flex justify-content-center">Notre équipe se tient à votre disposition pour <br> toutes demandes ou conseils, n'hésitez pas.</p>
+                    <div class="col-lg-6 imgCN">
+
                     </div>
 
                 </div>
@@ -26,54 +26,54 @@ head($page); ?>
             <div class="container">
                 <form>
                     <div class="row">
-                        <div class="form-group col-lg-6">
+                        <div class="form-group col-lg-6 mt-4">
                             <label for="nom"></label>
                             <input type="text" class="form-control" id="nom" placeholder="NOM*">
                         </div>
-                        <div class="form-group col-lg-6">
+                        <div class="form-group col-lg-6 mt-4">
                             <label for="prenom"></label>
                             <input type="text" class="form-control" id="prenom" placeholder="PRENOM*">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-lg-12">
+                        <div class="form-group col-lg-12 mt-4">
                             <label for="email"></label>
                             <input type="email" class="form-control" id="email" placeholder="EMAIL*">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-lg-12">
+                        <div class="form-group col-lg-12 mt-4">
                             <label for="telephone"></label>
                             <input type="tel" class="form-control" id="telephone" placeholder="TELEPHONE*">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="form-group col-lg-6">
+                        <div class="form-group col-lg-6 mt-4">
                             <label for="codePostal"></label>
                             <input type="text" class="form-control" id="codePostal" placeholder="CODE POSTAL*">
                         </div>
-                        <div class="form-group col-lg-6">
+                        <div class="form-group col-lg-6 mt-4">
                             <label for="ville"></label>
                             <input type="text" class="form-control" id="ville" placeholder="VILLE*">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-lg-12">
+                        <div class="form-group col-lg-12 mt-4">
                             <label for="societe"></label>
                             <input type="text" class="form-control" id="societe" placeholder="SOCIETE">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-lg-12">
+                        <div class="form-group col-lg-12 mt-4">
                             <label for="message"></label>
                             <textarea class="form-control" id="message" rows="6" placeholder="MESSAGE"></textarea>
                         </div>
                     </div>
                     <br>
                     <div class="row">
-                        <div class="form-group col-lg-12 pb-4">
-                            <button type="submit" class="btn btn-primary">Envoyer</button>
+                        <div class="form-group col-lg-12 pb-4 mt-4">
+                            <button type="submit" class="btn btn-light btn-lg">Envoyer</button>
                         </div>
                     </div>
                 </form>
