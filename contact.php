14acb6155aab5467923ed455ca1681969c9fbae9
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-12 10:00:37 +0100

    Update form

--- contact.php
+++ contact.php
@@ -22,49 +22,57 @@ head($page); ?>
         <section class="sectionForm bgForm">
             <div class="container">
                 <form>
-                    <div class="form-row">
+                    <div class="row">
                         <div class="form-group col-lg-6">
-                            <label for="nom">Nom</label>
-                            <input type="text" class="form-control" id="nom" placeholder="Votre nom">
+                            <label for="nom"></label>
+                            <input type="text" class="form-control" id="nom" placeholder="NOM*">
                         </div>
                         <div class="form-group col-lg-6">
-                            <label for="prenom">Prénom</label>
-                            <input type="text" class="form-control" id="prenom" placeholder="Votre prénom">
+                            <label for="prenom"></label>
+                            <input type="text" class="form-control" id="prenom" placeholder="PRENOM*">
                         </div>
                     </div>
-
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Votre email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="telephone">Téléphone</label>
-                        <input type="tel" class="form-control" id="telephone" placeholder="Votre téléphone">
-                    </div>
-
-                    <div class="form-row">
-                        <div class="form-group col-lg-6">
-                            <label for="codePostal">Code Postal</label>
-                            <input type="text" class="form-control" id="codePostal" placeholder="Code Postal">
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <label for="email"></label>
+                            <input type="email" class="form-control" id="email" placeholder="EMAIL*">
                         </div>
-                        <div class="form-group col-lg-6">
-                            <label for="ville">Ville</label>
-                            <input type="text" class="form-control" id="ville" placeholder="Ville">
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <label for="telephone"></label>
+                            <input type="tel" class="form-control" id="telephone" placeholder="TELEPHONE*">
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <label for="societe">Société</label>
-                        <input type="text" class="form-control" id="societe" placeholder="Votre société">
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label for="codePostal"></label>
+                            <input type="text" class="form-control" id="codePostal" placeholder="CODE POSTAL*">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label for="ville"></label>
+                            <input type="text" class="form-control" id="ville" placeholder="VILLE*">
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                        <label for="message">Message</label>
-                        <textarea class="form-control" id="message" rows="6" placeholder="Votre message"></textarea>
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <label for="societe"></label>
+                            <input type="text" class="form-control" id="societe" placeholder="SOCIETE">
+                        </div>
                     </div>
-
-                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <label for="message"></label>
+                            <textarea class="form-control" id="message" rows="6" placeholder="MESSAGE"></textarea>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="form-group col-lg-12">
+                            <button type="submit" class="btn btn-primary">Envoyer</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </section>
