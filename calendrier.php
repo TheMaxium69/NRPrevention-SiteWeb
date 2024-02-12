35749661927b58e88ab0f96fd65a8412dd865f3c
RayanDev23 <rayanquessada.rq@gmail.com>
2024-02-12 12:25:12 +0100

    conflit update

--- calendrier.php
+++ calendrier.php
@@ -7,6 +7,106 @@ head($page); ?>
     </header>
 
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<main>
+<section>
+            <!-- LE NOM DE MA SECTION EN COMMENTAIRE -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords) -->
+                
+                <div class="row d-flex justify-content-around"> <!-- ça aligne -->
+                    <div class="col-lg-2 titre-formation"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-6 contenu-formation "> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="d-flex justify-content-around">
+                            <div class="col-lg-4">
+                            <h4 class="d-flex justify-content-around mb-5">Dates</h4>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                            </div>
+                            <div class="col-lg-4">
+                            <h4 class="d-flex justify-content-around mb-5">Durée</h4>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                            </div>
+                            <div class="col-lg-4">
+                            <h4 class="d-flex justify-content-around mb-5">Horaires</h4>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                                <p>Le 17/08/2022 au 18/08/2022</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 lieu mb-5"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="">
+                            ici mon contenu
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    
+
+                    <div class="col-lg-2"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                    <div class="col-lg-4"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="border mb-3">
+                            ici mon contenu
+                        </div>
+                    </div>
+                </div>
+            </div>    
+</main>
+=======
+>>>>>>> Stashed changes
     <main>
         <div class="displayback-calendar">
         </div>
@@ -52,6 +152,10 @@ head($page); ?>
             </div>
         </div>
     </main>
+<<<<<<< Updated upstream
+=======
+>>>>>>> 556917ee12012a8304adcf339527c2e1778eb205
+>>>>>>> Stashed changes
 
 
 
